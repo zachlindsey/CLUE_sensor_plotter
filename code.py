d50d060f084c3a5b158a7d5077544bd5038d8d55
zachlindsey <lindsey.zachery@gmail.com>
2021-03-04 21:35:44 -0600

    scroll between two screens!
    should make it a bit nicer... but works! phew!
    
    1. it's not very responsive...
    2. it would be nice to have a flexible system of screens

--- code.py
+++ code.py
@@ -10,13 +10,31 @@ initial_title = "Welcome to\nCLUE Plotter"
 plotter = Plotter(board.DISPLAY, title=initial_title)
 
 plotter.display_on()
+state = 0
+button_lockout_timer = time.time()
 while True:
-	plotter.update_content(
-		temp = clue.temperature, 
-		pressure = clue.pressure,
-		prox = clue.proximity,
-		humid = clue.humidity,
-		color = clue.color
-	)
-	time.sleep(1)
+	if time.time() - button_lockout_timer > 0.1:
+		button_lockout_timer = time.time()
+		if clue.button_a:
+			state -= 1
+			state %= 2
+
+		if clue.button_b:
+			state += 1
+			state %= 2
+			
+
+	if state == 0:
+			plotter.update_content(
+				temp = clue.temperature, 
+				pressure = clue.pressure,
+				prox = clue.proximity,
+				humid = clue.humidity,
+				color = clue.color
+			)
+	elif state == 1:
+		plotter.test()
+
+
+
 
--- plotter.py
+++ plotter.py
@@ -1,5 +1,6 @@
 from adafruit_display_text.label import Label
 import terminalio
+import time
 import displayio
 
 def RGB_hex(R,G,B):
@@ -14,6 +15,8 @@ class Plotter:
 	PROX_SLOT = 3
 	HUMID_SLOT = 4
 	COLOR_SLOT = 5
+
+
 	LABEL_COLOR = 0xc0c0c0
 	def __init__(self, output, title="",
 		screen_width=240, screen_height=240):
@@ -24,14 +27,16 @@ class Plotter:
 		self._content = None
 		self._screen_width = screen_width
 		self._screen_height = screen_height
-
 		self._init_temp = False
+		self.time = time.time()
+
+
+		self._content = self._create_content()
+		self._test_content = self._create_test_content()
 
 
 
 	def display_on(self, tg_and_plot=None):
-		if self._content is None:
-			self._content = self._create_content()
 		self._output.show(self._content)
 
 	def get_temp_line(self, temp):
@@ -121,13 +126,38 @@ class Plotter:
 		content.append(self.get_color_line(0,0,0))
 
 
+
+
 		return content
 
 	def update_content(self, temp, pressure, prox, humid, color):
-		
+		if time.time() - self.time < 1:
+			return None
+		self.time = time.time()
 		self._content[self.TEMP_SLOT] = self.get_temp_line(temp)
 		self._content[self.PRESSURE_SLOT] = self.get_pressure_line(pressure)
 		self._content[self.PROX_SLOT] = self.get_proximity_line(prox)
 		self._content[self.HUMID_SLOT] = self.get_humidity_line(humid)
 		r,g,b, _ = color
 		self._content[self.COLOR_SLOT] = self.get_color_line(r,g,b)
+		self._output.show(self._content)
+
+	def test(self):
+		self._output.show(self._test_content)
+
+	def _create_test_content(self):
+		text = Label(
+			self._font,
+			x = 50,
+			y = 20,
+			text = "TEST",
+			max_glyphs = 4,
+			scale = 4,
+			line_spacing = 1,
+			color = self.LABEL_COLOR
+		)
+
+
+		content = displayio.Group(max_size = 1)
+		content.append(text)
+		return content
