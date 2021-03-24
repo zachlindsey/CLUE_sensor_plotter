feba7bd43b40adb5c77e92966feb2bf1a326664b
zachlindsey <lindsey.zachery@gmail.com>
2021-03-23 22:35:07 -0500

    messing with BLE
    sent some numbers from desktop to clue

--- code.py
+++ code.py
@@ -1,121 +1,168 @@
-import board
+# import board
+
+# import gc
+
+# from plotter import Plotter
+# from games import CenterGame
+
+from adafruit_ble import BLERadio, Advertisement
+from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
+from adafruit_ble.uuid import VendorUUID
+from adafruit_ble.characteristics.stream import StreamIn
+from adafruit_ble.services import Service
+
+# from adafruit_display_text.label import Label
+# import terminalio
 import time
+# import displayio
 
-import gc
+ble = BLERadio()
+ble.name = "CLUE BLE"
 
-from plotter import Plotter
-from games import CenterGame
+class CustomUART(Service):
 
-from adafruit_clue import clue
+    uuid = VendorUUID("8ba86973-935c-447c-91ad-bdcbad575f31")
+    _server_rx = StreamIn(
+        uuid=VendorUUID("8ba86974-935c-447c-91ad-bdcbad575f31"),
+        timeout=1.0,
+        buffer_size=64,
+    )
 
-from adafruit_display_text.label import Label
-import terminalio
-import time
-import displayio
+    def __init__(self, service=None):
+        # just steal the uuid code from MIDISerivce
+        
+        super().__init__(service = service)
+        self.connectable = True
+        self._rx = self._server_rx
 
-initial_title = "Welcome to\nCLUE Plotter"
-
-plotter = Plotter(board.DISPLAY)
-center_game = CenterGame(board.DISPLAY)
-
-state = 0
-
-class Source:
-    def __init__(self,
-        name,
-        clue_reading,
-        is_scalar
-    ):
-        self.name = name
-        self.data = []
-        self.data_pointer = 0
-        self.clue_reading = clue_reading
-        self.is_scalar = is_scalar
-
-    def update(self):
-        graph_window_size = 200
-        if len(self.data) < graph_window_size:
-            if self.is_scalar:
-                self.data.append([self.clue_reading()])
-            else:
-                self.data.append(self.clue_reading())
-        else:
-            if self.is_scalar:
-                self.data[self.data_pointer] = [self.clue_reading()]
-            else:
-                self.data[self.data_pointer] = self.clue_reading()
-
-        self.data_pointer += 1
-        self.data_pointer %= graph_window_size
+    def read(self, nbytes=None):
+        return self._rx.read(nbytes)
 
 
-sources = [
-    Source("temperature", lambda: clue.temperature, True),
-    Source("pressure", lambda : clue.pressure, True),
-    Source("accel", lambda: clue.acceleration, False),
-    Source("humidity", lambda: clue.humidity, True),
-    Source("touch_0", lambda: clue.touch_0, True)
-]
-num_screens = len(sources)
+service = CustomUART()
+ad = ProvideServicesAdvertisement(service)
+
 
 while True:
-    center_game.draw(clue.acceleration)
+    if ble.connected:
+        print('connected!' + service.read(1).decode('utf-8'))
 
-last_source = ''
-while True:
-    gc.collect() 
-    print(gc.mem_free())
+    else:
+        print('not connected...')
+        if not ble.advertising:
+            ble.start_advertising(ad)
+    time.sleep(1)
 
-    for source in sources:
-        source.update()
 
-    if clue.button_a:
-        state -= 1
-        state %= num_screens
-        changed = True
-        time.sleep(0.1)
+#
+#   BLE Broadcast Measurements
+#
 
-    elif clue.button_b:
-        state += 1
-        state %= num_screens
-        changed = True
-        time.sleep(0.1)
+# import time
+# from adafruit_clue import clue
+# import adafruit_ble_broadcastnet
+
+# while True:
+#     measurement = adafruit_ble_broadcastnet.AdafruitSensorMeasurement()
+
+#     measurement.temperature = clue.temperature
+#     measurement.pressure = clue.pressure
+#     measurement.relative_humidity = clue.humidity
+#     measurement.acceleration = clue.acceleration
+#     measurement.magnetic = clue.magnetic
+
+#     print(measurement)
+#     print(measurement.data_dict[255])
+#     adafruit_ble_broadcastnet.broadcast(measurement)
+#     time.sleep(60)
+
+
+
+
+#
+# GAME
+#
+
+
+# center_game = CenterGame(board.DISPLAY)
+
+
+# while True:
+#     center_game.draw(clue.acceleration)
+
+
+#
+# SENSORS
+#
+
+
+# initial_title = "Welcome to\nCLUE Plotter"
+
+# plotter = Plotter(board.DISPLAY)
+
+# state = 0
+
+# class Source:
+#     def __init__(self,
+#         name,
+#         clue_reading,
+#         is_scalar
+#     ):
+#         self.name = name
+#         self.data = []
+#         self.data_pointer = 0
+#         self.clue_reading = clue_reading
+#         self.is_scalar = is_scalar
+
+#     def update(self):
+#         graph_window_size = 200
+#         if len(self.data) < graph_window_size:
+#             if self.is_scalar:
+#                 self.data.append([self.clue_reading()])
+#             else:
+#                 self.data.append(self.clue_reading())
+#         else:
+#             if self.is_scalar:
+#                 self.data[self.data_pointer] = [self.clue_reading()]
+#             else:
+#                 self.data[self.data_pointer] = self.clue_reading()
+
+#         self.data_pointer += 1
+#         self.data_pointer %= graph_window_size
+
+
+# sources = [
+#     Source("temperature", lambda: clue.temperature, True),
+#     Source("pressure", lambda : clue.pressure, True),
+#     Source("accel", lambda: clue.acceleration, False),
+#     Source("humidity", lambda: clue.humidity, True),
+#     Source("touch_0", lambda: clue.touch_0, True)
+# ]
+# num_screens = len(sources)
+
+# last_source = ''
+# while True:
+#     gc.collect() 
+#     print(gc.mem_free())
+
+#     for source in sources:
+#         source.update()
+
+#     if clue.button_a:
+#         state -= 1
+#         state %= num_screens
+#         changed = True
+#         time.sleep(0.1)
+
+#     elif clue.button_b:
+#         state += 1
+#         state %= num_screens
+#         changed = True
+#         time.sleep(0.1)
             
-    data = sources[state].data
-    data_pointer = sources[state].data_pointer
-    name = sources[state].name
+#     data = sources[state].data
+#     data_pointer = sources[state].data_pointer
+#     name = sources[state].name
 
-    plotter.draw_graph(name, data, data_pointer)
+#     plotter.draw_graph(name, data, data_pointer)
 
-
-
-
-
-
-
-#
-# This is some code from the site that shows off
-# all the features of the CLUE!
-#
-
-
-# clue.sea_level_pressure = 1020
-
-# clue_data = clue.simple_text_display(title="CLUE Sensor Data!", title_scale=2)
-
-        # clue_data[0].text = "Acceleration: {:.2f} {:.2f} {:.2f}".format(*clue.acceleration)
-        # clue_data[1].text = "Gyro: {:.2f} {:.2f} {:.2f}".format(*clue.gyro)
-        # clue_data[2].text = "Magnetic: {:.3f} {:.3f} {:.3f}".format(*clue.magnetic)
-        # clue_data[3].text = "Pressure: {:.3f}hPa".format(clue.pressure)
-        # clue_data[4].text = "Altitude: {:.1f}m".format(clue.altitude)
-        # clue_data[5].text = "Temperature: {:.1f}C".format(clue.temperature)
-        # clue_data[6].text = "Humidity: {:.1f}%".format(clue.humidity)
-        # clue_data[7].text = "Proximity: {}".format(clue.proximity)
-        # clue_data[8].text = "Gesture: {}".format(clue.gesture)
-        # clue_data[9].text = "Color: R: {} G: {} B: {} C: {}".format(*clue.color)
-        # clue_data[10].text = "Button A: {}".format(clue.button_a)
-        # clue_data[11].text = "Button B: {}".format(clue.button_b)
-        # clue_data[12].text = "Touch 0: {}".format(clue.touch_0)
-        # clue_data[13].text = "Touch 1: {}".format(clue.touch_1)
-        # clue_data[14].text = "Touch 2: {}".format(clue.touch_2)
-        # clue_data.show()
---
+++ desktop_app.py
@@ -0,0 +1,46 @@
+import asyncio
+import time
+from bleak import BleakScanner, BleakClient
+from bleak.backends._manufacturers import MANUFACTURERS
+
+
+async def scan_for_devices():
+    devices = await BleakScanner.discover()
+    for d in devices:
+        print('---')
+        print('Name:\t', d.name)
+        print('Address:\t',d.address)
+        print('RSSI:\t', d.rssi)
+        print('UUIDS:')
+        for uuid in d.metadata['uuids']:
+            print('\t'+str(uuid))
+        if "manufacturer_data" in d.metadata:
+            ks = list(d.metadata["manufacturer_data"].keys())
+            if len(ks):
+                mf = MANUFACTURERS.get(ks[0], MANUFACTURERS.get(0xFFFF))
+                value = d.metadata["manufacturer_data"].get(
+                    ks[0], MANUFACTURERS.get(0xFFFF)
+                )
+                # TODO: Evaluate how to interpret the value of the company identifier...
+                print("{0} ({1})".format(mf, value))
+
+ADDRESS = "C5:DF:52:61:6F:19"
+async def run(address):
+    client = BleakClient(address)
+    try:
+        await client.connect()
+        print("Connceted!")
+        rx_char = client.services.get_characteristic("8ba86974-935c-447c-91ad-bdcbad575f31")
+        for i in range(0,10):
+            await client.write_gatt_char(rx_char, str(i).encode('utf-8'))
+            time.sleep(1)
+
+    except Exception as e:
+        print(e)
+    finally:
+        await client.disconnect()
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(run(ADDRESS))
+
+
